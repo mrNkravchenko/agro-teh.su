5066b8d9af6b9605555f5132ffff412fc7fb0a2f
Nikita Kravchenko <nikita.kravchenko@umbellait.com>
2019-06-22 22:30:41 +0300

    #new_dev hotfix for soareparts

--- assets/js/app.js
+++ assets/js/app.js
@@ -1035,6 +1035,7 @@ $(document).ready(()=>{
     }
     function errorFilter(value, elem) {
         console.log(value);
+        $('#innerContent').html();
         $(elem).removeClass('btn-animation');
         $(elem).children().prop('disabled', false);
     }
